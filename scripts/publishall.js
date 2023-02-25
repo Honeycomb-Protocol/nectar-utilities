87edb1dd8eff9297a6a3603bba9d7e325e402c49
sultandilaram <sultanndilaram@gmail.com>
2023-02-25 17:05:04 +0500

    minor changes

--- scripts/publishall.js
+++ scripts/publishall.js
@@ -6,7 +6,10 @@ console.log(process.env.NODE_AUTH_TOKEN)
 // write the version in cargo.toml of all programs
 const packages = fs.readdirSync("packages");
 packages.forEach((package) => {
-  execSync(`cp ../../.npmrc .npmrc && npm publish --access public`, {
+  execSync(`ls && cp .npmnrc ./dist/${package}/.npmrc`, {
+    stdio: 'inherit'
+  })
+  execSync(`npm publish --access public`, {
     cwd: `dist/${package}`,
     stdio: 'inherit'
   });
