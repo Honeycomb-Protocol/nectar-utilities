076daf9a75087d78646da0929710b4751a134e0f
owais-star <iqbalowais35@gmail.com>
2023-10-03 22:17:37 +0500

    Feat: add guild in NftUsedBy to keep track if the Nft is in use by Guild Program

--- programs/hpl-nectar-staking/src/state/nft_state.rs
+++ programs/hpl-nectar-staking/src/state/nft_state.rs
@@ -1,58 +1,5 @@
 use {anchor_lang::prelude::*, hpl_utils::traits::*};
 
-/// The staking account linked to the NFT
-/// PDA: ['nft', mint, staking_pool]
-/// Category: nft_state
-#[account]
-#[derive(Debug)]
-pub struct NFT {
-    pub bump: u8,
-
-    /// The staking_pool this NFT is staked in
-    pub staking_pool: Pubkey,
-
-    /// wallet of the staker
-    pub staker: Pubkey,
-
-    /// The mint of the NFT
-    pub mint: Pubkey,
-
-    /// The verified creator of the NFT
-    pub creator: Pubkey,
-
-    /// The verified collection of the NFT
-    pub collection: Pubkey,
-
-    /// Last time the owner claimed rewards
-    pub last_claim: i64,
-
-    /// Accumulated staked at
-    pub staked_at: i64,
-
-    /// Last staked at
-    pub last_staked_at: i64,
-
-    /// Last unstraked_at
-    pub last_unstaked_at: i64,
-}
-
-impl Default for NFT {
-    const LEN: usize = 8 + 200;
-
-    fn set_defaults(&mut self) {
-        self.bump = 0;
-        self.staking_pool = Pubkey::default();
-        self.staker = Pubkey::default();
-        self.mint = Pubkey::default();
-        self.creator = Pubkey::default();
-        self.collection = Pubkey::default();
-        self.last_claim = 0;
-        self.staked_at = 0;
-        self.last_staked_at = 0;
-        self.last_unstaked_at = 0;
-    }
-}
-
 // The staking account linked to the NFT
 /// PDA: ['nft', mint, staking_pool]
 /// Category: nft_state
@@ -122,4 +69,10 @@ pub enum NFTCriteria {
 pub enum NFTUsedBy {
     None,
     Missions,
+    Guild { id: Pubkey, role: GuildRole },
+}
+
+pub enum GuildRole {
+    Chief,
+    Member,
 }
