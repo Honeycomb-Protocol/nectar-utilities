d8ee5b4ee8e6b257c713c18961eb457abf0ed3d7
Ghulam Mustafa Raza Qadri <ghulammustafasoomar@gmail.com>
2023-10-04 02:03:45 +0500

    WIP

--- programs/hpl-nectar-missions/src/lib.rs
+++ programs/hpl-nectar-missions/src/lib.rs
@@ -8,39 +8,7 @@ pub mod utils;
 declare_id!("HuntaX1CmUt5EByyFPE8pMf13SpvezybmMTtjmpmGmfj");
 
 use instructions::*;
-
-fn platform_gate_fn<'info>(
-    action: hpl_hive_control::state::SerializableActions,
-    service: Option<(u8, Pubkey)>,
-    project: AccountInfo<'info>,
-    signer: AccountInfo<'info>,
-    payer: AccountInfo<'info>,
-    vault: AccountInfo<'info>,
-    delegate_authority: &Option<Account<'info, hpl_hive_control::state::DelegateAuthority>>,
-    system_program: AccountInfo<'info>,
-    hive_control: AccountInfo<'info>,
-    instructions_sysvar: AccountInfo<'info>,
-) -> Result<()> {
-    hpl_hive_control::cpi::platform_gate(
-        CpiContext::new(
-            hive_control.to_account_info(),
-            hpl_hive_control::cpi::accounts::PlatformGate {
-                project: project,
-                delegate_authority: if let Some(delegate_authority) = &delegate_authority {
-                    Some(delegate_authority.to_account_info())
-                } else {
-                    Some(hive_control)
-                },
-                signer,
-                payer,
-                system_program,
-                instructions_sysvar,
-                vault: vault.to_account_info(),
-            },
-        ),
-        hpl_hive_control::instructions::PlatformGateArgs { action, service },
-    )
-}
+hpl_macros::platform_gate!();
 
 #[program]
 pub mod hpl_nectar_missions {
@@ -50,35 +18,9 @@ pub mod hpl_nectar_missions {
         ctx: Context<CreateMissionPool>,
         args: CreateMissionPoolArgs,
     ) -> Result<()> {
-        hpl_hive_control::cpi::add_remove_service(
-            CpiContext::new(
-                ctx.accounts.hive_control.to_account_info(),
-                hpl_hive_control::cpi::accounts::AddRemoveService {
-                    project: ctx.accounts.project.to_account_info(),
-                    delegate_authority: if let Some(delegate_authority) =
-                        &ctx.accounts.delegate_authority
-                    {
-                        Some(delegate_authority.to_account_info())
-                    } else {
-                        Some(ctx.accounts.hive_control.to_account_info())
-                    },
-                    authority: ctx.accounts.authority.to_account_info(),
-                    payer: ctx.accounts.payer.to_account_info(),
-                    system_program: ctx.accounts.system_program.to_account_info(),
-                    hpl_events: ctx.accounts.hpl_events.to_account_info(),
-                    clock: ctx.accounts.clock_sysvar.to_account_info(),
-                    rent_sysvar: ctx.accounts.rent_sysvar.to_account_info(),
-                    instructions_sysvar: ctx.accounts.instructions_sysvar.to_account_info(),
-                    vault: ctx.accounts.vault.to_account_info(),
-                },
-            ),
-            hpl_hive_control::instructions::AddRemoveServiceArgs {
-                service: hpl_hive_control::state::Service::Missions {
-                    pool_id: ctx.accounts.mission_pool.key(),
-                },
-                remove: Some(false),
-            },
-        )?;
+        hpl_macros::add_service!(hpl_hive_control::state::Service::Missions {
+            pool_id: ctx.accounts.mission_pool.key(),
+        });
 
         instructions::create_mission_pool(ctx, args)
     }
